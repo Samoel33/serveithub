70be467e68ed7486bdc4c708fc2368114ed28ee5
Samoel33 <68919054+Samoel33@users.noreply.github.com>
2023-12-30 22:53:01 +0200

    Update GetAQuote.tsx

--- src/Components/GetAQuote.tsx
+++ src/Components/GetAQuote.tsx
@@ -38,7 +38,7 @@ export default function GetAquoteComponent(){
     
   const {register,handleSubmit,reset,formState} = useForm<emailUs>()
   const {errors} =formState
-     const form = useRef<HTMLFormElement>();
+     const form = useRef<HTMLFormElement|undefined|string>();
   const  sendEmail = async(data:emailUs)=>{
     reset();
     emailjs.sendForm(
